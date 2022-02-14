88d1af80e338fe367ec754f17d9e1440d3c47931
Miguel Martinez <miguel.emezero@gmail.com>
2022-02-13 19:40:57 -0600

    file(Root): a file to export all icons

---
+++ src/components/icons.js
@@ -0,0 +1,12 @@
+export { FaPlayCircle as PlayCircle } from 'react-icons/fa'
+export { BsFillSkipStartFill as SkipStart, BsSkipEndFill as SkipEnd, BsSearch as Search } from 'react-icons/bs'
+export { BiShuffle as Shuffle, BiRepeat as Repeat, BiLibrary as Library } from 'react-icons/bi'
+
+export { GiMicrophone as Microphone } from 'react-icons/gi'
+export { MdOutlineQueueMusic as QueueMusic, MdDevicesOther as DevicesOther } from 'react-icons/md'
+export { CgVolume as Volume } from 'react-icons/cg'
+
+export { ImUser as User } from 'react-icons/im'
+export { IoMdSearch } from 'react-icons/io'
+
+export { GrHomeRounded as Home } from 'react-icons/gr'
